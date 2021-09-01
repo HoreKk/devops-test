4f870b48ffcbfcff0e5954dd03f7bb3eaf9ef121
Antoine Lelong <azenoxe@gmail.com>
2021-09-01 15:57:25 +0200

    fix: message in HelloWorld component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Clément" />
+  <HelloWorld msg="Hello Yoann" />
 </template>
 
 <style>
